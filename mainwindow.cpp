491615657e83dce0ea820a6752f62b591fd207c6
lathief <shadowmaster241099@yahoo.co.id>
2023-05-16 15:50:43 +0700

    change default font

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QFont font("Ubuntu Reguler");
+    font.setWeight(QFont::Normal);
+    QApplication::setFont(font);
     this->setCentralWidget(ui->textEdit);
 }
 
